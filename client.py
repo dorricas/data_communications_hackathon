8b1837699fcd70202f8930de135c0081695e1716
dorricas <casdorri@gmail.com>
2025-01-15 22:18:09 +0200

    tcp_connect_using_offer_data

--- client.py
+++ client.py
@@ -1,7 +1,26 @@
+import time
 import socket
 import struct
+import threading
+from queue import Queue
 
 EXPECTED_MAGIC_CO0KIE = 0xabcddcba
+
+
+port_pool = Queue()
+for port in range(50000, 50100):  # Example port range for UDP connections
+    port_pool.put(port)
+
+
+def get_port():
+    """Get a port from the port pool."""
+    return port_pool.get()
+
+
+def release_port(port):
+    """Release a port back to the port pool."""
+    port_pool.put(port)
+
 
 
 def start_client():
@@ -23,52 +42,92 @@ def start_client():
     #     print("Closing connection")
     #     client_socket.close()
 
-    client_name = input("Enter client name: ")
+
+    file_size = int(input("please input file size: "))
+    tcp_conn_num = int(input("please input TCP connection number: "))
+    udp_conn_num = int(input("please input UDP connection number: "))
 
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     udp_socket.bind(("", 12345))  # Bind to the broadcast port
 
-
-
-    print("Listening for server offers...")
+    print("Client started, listening for offer requests...")
 
     expected_message_type = 0x2
 
     while True:
         data, server_address = udp_socket.recvfrom(1024)
         try:
-            magic_cookie, message_type, udp_port, tcp_port = struct.unpack('!I B H H', data)
+            magic_cookie, message_type, server_udp_port, server_tcp_port = struct.unpack('!I B H H', data)
             if magic_cookie == EXPECTED_MAGIC_CO0KIE and message_type == expected_message_type:
-                print(f"Received offer from {server_address}: UDP port {udp_port}, TCP port {tcp_port}")
-                while True:
-                    to_connect = input("Do you want to connect to server? (y/n): ")
-                    if to_connect == "y":
-                        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                        server_ip, _ = server_address
-                        client_socket.bind(('', tcp_port))
-                        print((server_ip, tcp_port))
-                        client_socket.connect((server_ip, tcp_port))
+                print(f"Received offer from {server_address[0]}")
 
-                        print('Connected to server')
-                        message = f"Hello! I'm client {client_name}"
-                        client_socket.sendall(message.encode())
-
-                        data = client_socket.recv(1024)
-                        print(data.decode())
-
-                        print('Closing connection')
-                        client_socket.close()
-
-
-
-                    if to_connect == "n":
-                        break
+                tcp_thread = threading.Thread(target=tcp_connection, args=(server_tcp_port, server_address[0]))
+                tcp_thread.start()
+                # while True:
+                #     to_connect = input("Do you want to connect to server? (y/n): ")
+                #     if to_connect == "y":
+                #         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                #         server_ip, _ = server_address
+                #         client_socket.bind(('', tcp_port))
+                #         print((server_ip, tcp_port))
+                #         client_socket.connect((server_ip, tcp_port))
+                #
+                #         print('Connected to server')
+                #         message = f"Hello! I'm client {client_name}"
+                #         client_socket.sendall(message.encode())
+                #
+                #         data = client_socket.recv(1024)
+                #         print(data.decode())
+                #
+                #         print('Closing connection')
+                #         client_socket.close()
+                #
+                #
+                #
+                #     if to_connect == "n":
+                #         break
 
 
 
         except struct.error:
             print("Received malformed offer message.")
+
+def tcp_connection(server_port, server_ip):
+    """Handle a single TCP connection."""
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_socket:
+            print((server_ip, server_port))
+            tcp_socket.connect((server_ip, server_port))
+            print(f"TCP connection established with {server_ip}:{server_port}")
+
+            while True:
+                message = "Hello, TCP server!"
+                tcp_socket.sendall(message.encode())
+                response = tcp_socket.recv(1024)
+                print(f"Received from TCP server: {response.decode()}")
+                time.sleep(2)  # Simulate periodic communication
+    except Exception as e:
+        print(f"TCP connection error: {e}")
+
+
+def udp_connection(server_port, server_ip):
+    source_port = get_port()  # Get a port from the pool
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_socket:
+            udp_socket.bind(("", source_port))
+            print(f"UDP connection bound to port {source_port}")
+
+            while True:
+                message = "Hello, UDP server!"
+                udp_socket.sendto(message.encode(), (server_ip, server_port))
+                response, addr = udp_socket.recvfrom(1024)
+                print(f"Received from UDP server: {response.decode()}")
+                time.sleep(2)  # Simulate periodic communication
+    except Exception as e:
+        print(f"UDP connection error: {e}")
+    finally:
+        release_port(source_port)  # Release the port back to the pool
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -3,46 +3,86 @@ import socket
 import struct
 import threading
 
+
+TCP_PORT = 65431
+UDP_PORT = 65430
+OFFER_PORT = 12345
+
+BROADCAST = '<broadcast>'
+LOCALHOST = "localhost"
+ALL_NETWORK_IP ="0.0.0.0"
+
 def start_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = ('localhost', 65431)
-    server_socket.bind(server_address)
-    server_socket.listen(1)
+    # server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # server_address = ('localhost', OFFER_PORT)
+    # server_socket.bind(server_address)
+    # server_socket.listen(1)
+    #
+    # print(f"Server is running on {server_address[0]}:{server_address[1]}")
+    #
+    # # Start the UDP broadcast thread
+    # udp_thread = threading.Thread(target=send_udp_broadcast, args=(UDP_PORT, TCP_PORT), daemon=True)
+    # udp_thread.start()
+    #
+    # send_udp_broadcast()
+    #
+    # try:
+    #     while True:
+    #         print("Waiting for a connection...")
+    #         connection, client_address = server_socket.accept()
+    #         try:
+    #             print(f"Connection established with {client_address}")
+    #
+    #             while True:
+    #                 data = connection.recv(1024)
+    #                 if data:
+    #                     print(f"Received: {data.decode()}")
+    #                     response = f"Server received: {data.decode()}"
+    #                     connection.sendall(response.encode())
+    #                 else:
+    #                     print("No more data from", client_address)
+    #                     break
+    #         finally:
+    #             connection.close()
+    #
+    # except KeyboardInterrupt:
+    #     print("\nKeyboard interrupt, server is shutting down.")
 
-    print(f"Server is running on {server_address[0]}:{server_address[1]}")
+    offer_thread = threading.Thread(target=send_udp_broadcast, args=(UDP_PORT, TCP_PORT), daemon=True)
+    offer_thread.start()
 
-    # Start the UDP broadcast thread
-    udp_thread = threading.Thread(target=send_udp_broadcast, args=(12345, server_address[1]), daemon=True)
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.bind((ALL_NETWORK_IP, TCP_PORT))
+    tcp_socket.listen(1)  # Backlog of 5 connections
+    print(f"TCP server is listening on {ALL_NETWORK_IP}:{TCP_PORT}")
+
+    # UDP socket setup
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind((LOCALHOST, UDP_PORT))
+    print(f"UDP server is listening on {LOCALHOST}:{UDP_PORT}")
+
+    # Start threads for each protocol
+    tcp_thread = threading.Thread(target=handle_tcp_connections, args=(tcp_socket,), daemon=True)
+    udp_thread = threading.Thread(target=handle_udp_connections, args=(udp_socket,), daemon=True)
+
+    tcp_thread.start()
     udp_thread.start()
 
     try:
+        # Keep the main thread alive to allow background threads to run
         while True:
-            print("Waiting for a connection...")
-            connection, client_address = server_socket.accept()
-            try:
-                print(f"Connection established with {client_address}")
-
-                while True:
-                    data = connection.recv(1024)
-                    if data:
-                        print(f"Received: {data.decode()}")
-                        response = f"Server received: {data.decode()}"
-                        connection.sendall(response.encode())
-                    else:
-                        print("No more data from", client_address)
-                        break
-            finally:
-                connection.close()
-
+            pass
     except KeyboardInterrupt:
-        print("\nKeyboard interrupt, server is shutting down.")
+        print("\nServer shutting down.")
+        # tcp_socket.close()
+        # udp_socket.close()
 
 
 # Function to send UDP broadcast
 def send_udp_broadcast(udp_port, tcp_port):
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    broadcast_address = ('<broadcast>', 12345)
+    broadcast_address = (BROADCAST, OFFER_PORT)
 
     magic_cookie = 0xabcddcba
     message_type = 0x2
@@ -53,6 +93,71 @@ def send_udp_broadcast(udp_port, tcp_port):
         udp_socket.sendto(message, broadcast_address)
         print(f"Broadcasted offer message with UDP port {udp_port} and TCP port {tcp_port}")
         time.sleep(1)
+
+
+    # udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    # broadcast_address = ('<broadcast>', 12345)
+    #
+    # magic_cookie = 0xabcddcba
+    # message_type = 0x2
+    #
+    # while True:
+    #     # Construct the offer message
+    #     message = struct.pack('!I B H H', magic_cookie, message_type, udp_port, tcp_port)
+    #     udp_socket.sendto(message, broadcast_address)
+    #     print(f"Broadcasted offer message with UDP port {udp_port} and TCP port {tcp_port}")
+    #     time.sleep(1)
+
+def handle_tcp_connections(tcp_socket):
+    """Handle incoming TCP connections."""
+    try:
+        while True:
+            print("Waiting for a TCP connection...")
+            connection, client_address = tcp_socket.accept()
+            print(f"TCP connection established with {client_address}")
+            # threading.Thread(target=handle_single_tcp_client, args=(connection, client_address), daemon=True).start()
+
+            data = connection.recv(1024)
+            if data:
+                print(f"Received from TCP client {client_address}: {data.decode()}")
+                response = f"Server received: {data.decode()}"
+                connection.sendall(response.encode())
+            else:
+                print(f"TCP client {client_address} disconnected.")
+                break
+    except Exception as e:
+        print(f"Error in TCP handler: {e}")
+
+
+def handle_single_tcp_client(connection, client_address):
+    """Handle communication with a single TCP client."""
+    try:
+        with connection:
+            while True:
+                data = connection.recv(1024)
+                if data:
+                    print(f"Received from TCP client {client_address}: {data.decode()}")
+                    response = f"Server received: {data.decode()}"
+                    connection.sendall(response.encode())
+                else:
+                    print(f"TCP client {client_address} disconnected.")
+                    break
+    except Exception as e:
+        print(f"Error with TCP client {client_address}: {e}")
+
+
+def handle_udp_connections(udp_socket):
+    """Handle incoming UDP messages."""
+    try:
+        while True:
+            data, client_address = udp_socket.recvfrom(1024)
+            print(f"Received from UDP client {client_address}: {data.decode()}")
+            response = f"Server received: {data.decode()}"
+            udp_socket.sendto(response.encode(), client_address)
+    except Exception as e:
+        print(f"Error in UDP handler: {e}")
+
 
 
 if __name__ == "__main__":
